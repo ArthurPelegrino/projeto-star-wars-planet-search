9697726a064b5f42d62594a4bb7f29eee2a4e692
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-01-12 11:32:35 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import TableContext from './context/TableContext';
+import Table from './components/Table';
 import './App.css';
 
 function App() {
+  const [myTable, setMyTable] = useState([]);
+
+  useEffect(() => {
+    fetch('https://swapi.dev/api/planets')
+      .then((response) => response.json())
+      .then((data) => {
+        setMyTable(data.results);
+      });
+  }, []);
+
+  myTable.map((elemento) => delete elemento.residents);
+
+  const context = {
+    myTable,
+  };
   return (
-    <span>Hello, App!</span>
+    <TableContext.Provider value={ context }>
+      <h1> StarWars</h1>
+      <Table />
+    </TableContext.Provider>
+
   );
 }
 
---
+++ src/components/Table.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, useContext } from 'react';
+import TableContext from '../context/TableContext';
+
+function Table() {
+  const { myTable } = useContext(TableContext);
+  return (
+    <>
+      <h4> Table </h4>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            myTable.map((info) => (
+              <tr key={ info.url }>
+                <td>{info.name}</td>
+                <td>{info.rotation_period}</td>
+                <td>{info.orbital_period}</td>
+                <td>{info.diameter}</td>
+                <td>{info.climate}</td>
+                <td>{info.gravity}</td>
+                <td>{info.terrain}</td>
+                <td>{info.surface_water}</td>
+                <td>{info.population}</td>
+                <td>{info.films}</td>
+                <td>{info.created}</td>
+                <td>{info.edited}</td>
+                <td>{info.url}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+      <button
+        type="button"
+        onClick={ console.log(myTable) }
+      >
+        console
+      </button>
+    </>
+  );
+}
+
+export default Table;
